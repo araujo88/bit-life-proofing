7607caa6fcc8bb1a4d126fc27040778b2a8a0356
Leonardo Araujo <leonardo.aa88@gmail.com>
2023-11-24 05:34:46 -0300

    fix: increased example interval to 1 min

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from recorder import Recorder
 
 if __name__ == "__main__":
     # Usage example
-    recorder = Recorder(check_in_interval='1 second', 
+    recorder = Recorder(check_in_interval='1 minute', 
                         pgp_fingerprint="F277A34924BBF522735D912A0F32F636EEB86B44",
                         gpg_passphrase="johndoeINSECURE*")
     recorder.start_monitoring()
-
